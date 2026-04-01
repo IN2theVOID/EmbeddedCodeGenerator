a207e651ca25ccb1919a7866da53b580c93a0ca6
in2TheVoid <in2thevoid@MacBook-Air>
2026-04-01 15:27:11 +0300

    add prometheus metrics

--- modules/controller.py
+++ modules/controller.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from modules.api.auth import auth_router
 from modules.api.admin import admin_router
@@ -16,3 +17,5 @@ controller.include_router(admin_router)
 controller.include_router(dashboard_router)
 controller.include_router(generator_router)
 controller.include_router(audit_router)
+
+Instrumentator().instrument(controller).expose(controller)
